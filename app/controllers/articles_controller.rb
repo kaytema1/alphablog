a48192a2065f52b6d65d3c334dc1bbdf3dcc7d58
Emmanuel Addo-Yirenkyi <eyirenkyi@claimsync.com>
2016-05-11 14:38:49 +0000

    Committing UnStyled Code

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,65 @@
+class ArticlesController < ApplicationController
+	#before_action: :article_params, only: 
+	def index
+		@articles = Article.all
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		
+		if @article.save
+			flash[:notice] = "Article was created successfuly"
+			redirect_to article_path(@article)
+		else
+			flash.now[:alert] = "Article was not created successfuly"
+			render 'new'
+		end
+
+	end 
+
+	def show
+		@article = Article.find(params[:id])
+
+	end
+
+	def edit
+		@article = Article.find { params[:id]}
+	end
+
+
+
+	def update
+		@article = Article.find { params[:id]}
+		if @article.update(article_params)
+			flash[:notice] = "Article was successfully updated"
+			redirect_to article_path(@article)
+		else
+			flash.now[:alert] = "Article was not updated successfully"
+			render 'edit'
+		end
+
+	end
+
+	def destroy
+		@article = Article.find{ params[:id]}
+		@article.destroy
+		flash[:notice] = "Article was deleted successfully"
+		redirect_to articles_path
+	end
+
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :description)
+	end
+
+	def set_article
+		@article = Article.find { params[:id]}
+	end
+
+end
